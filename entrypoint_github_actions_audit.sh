ab356c9c37963b5fd64d3a867031d421f5e4d22f
cr0hn <cr0hn@cr0hn.com>
2023-10-26 15:34:34 +0200

    fix - secret

--- entrypoint_github_actions_audit.sh
+++ entrypoint_github_actions_audit.sh
@@ -58,26 +58,28 @@ discovery_run() {
 
   for FILE in $JSON_FILES $YAML_FILES; do
 
+    FULL_PATH=$(pwd)/$FILE
+
     #
     # Check if file is a valid OpenAPI file
     #
-    if ! $VALIDATE_OPENAPI_BINARY "$FILE"; then
-      echo "[!] $FILE is not a valid OpenAPI file"
+    if ! $VALIDATE_OPENAPI_BINARY "$FULL_PATH"; then
+      echo "[!] $FULL_PATH is not a valid OpenAPI file"
       continue
     fi
 
-    AUDIT_REPORT="$FILE.report"
+    AUDIT_REPORT="$FULL_PATH.report"
 
-    echo "[*] Analyzing '$FILE'"
+    echo "[*] Analyzing '$FULL_PATH'"
 
     # Run 42Crunch Audit
     echo "    > Running 42Crunch Audit"
-    $AST_BINARY audit run --enrich=false --org "$GITHUB_REPOSITORY" --user "$GITHUB_REPOSITORY_OWNER" --output-format json -o "$AUDIT_REPORT" "$FILE" > /dev/null 2>&1
-#    $AST_BINARY audit run --enrich=false --org "$GITHUB_REPOSITORY" --user "$GITHUB_REPOSITORY_OWNER" --output-format json -o "$AUDIT_REPORT" "$FILE"
+#    $AST_BINARY audit run --enrich=false --org "$GITHUB_REPOSITORY" --user "$GITHUB_REPOSITORY_OWNER" --output-format json -o "$AUDIT_REPORT" "$FULL_PATH" > /dev/null 2>&1
+    $AST_BINARY audit run --enrich=false --org "$GITHUB_REPOSITORY" --user "$GITHUB_REPOSITORY_OWNER" --output-format json -o "$AUDIT_REPORT" "$FULL_PATH"
 
     # Convert o SARIF format
     echo "    > Converting $AUDIT_REPORT to SARIF format"
-    $CONVERT_TO_SARIF_BINARY -a "$FILE" -i "$AUDIT_REPORT" -o "$AUDIT_REPORT.sarif"
+    $CONVERT_TO_SARIF_BINARY -a "$FULL_PATH" -i "$AUDIT_REPORT" -o "$AUDIT_REPORT.sarif"
 
     # If the previous execution was successful then stop the loop
     if [ $? -eq 0 ]; then
@@ -95,39 +97,6 @@ discovery_run() {
   done
 }
 
-file_oriented_run() {
-  #
-  # This runs with a specific file
-  #
-  FILE=${INPUT_OPENAPI_FILE}
-  AUDIT_REPORT="$FILE.report"
-
-  echo "[*] Analyzing '$FILE'"
-
-  # Run 42Crunch Audit
-  echo "    > Running 42Crunch Audit"
-#  $AST_BINARY audit run --enrich=false --org "$GITHUB_REPOSITORY" --user "$GITHUB_REPOSITORY_OWNER" --output-format json -o "$AUDIT_REPORT" "$FILE" > /dev/null 2>&1
-  $AST_BINARY audit run --enrich=false --org "$GITHUB_REPOSITORY" --user "$GITHUB_REPOSITORY_OWNER" --output-format json -o "$AUDIT_REPORT" "$FILE"
-
-  # Convert o SARIF format
-  echo "    > Converting $AUDIT_REPORT to SARIF format"
-  $CONVERT_TO_SARIF_BINARY -a "$FILE" -i "$AUDIT_REPORT" -o "$AUDIT_REPORT.sarif"
-
-  # If the previous execution was successful then stop the loop
-  if [ $? -eq 0 ]; then
-
-    # Scanning and generating sarif success. Now we need to upload the report to scanning code if the is set
-    echo "Before"
-    if [ -n "$USER_UPLOAD_TO_SCANNING_CODE" ]; then
-      echo "After"
-
-      # Upload report to GitHub scanning code
-      echo "    > Uploading $AUDIT_REPORT.sarif to GitHub scanning code"
-      $UPLOAD_SARIF_BINARY --github-token "$USER_TOKEN" --github-repository "$GITHUB_REPOSITORY" --github-sha "$GITHUB_SHA" --ref "$GITHUB_REF" "$AUDIT_REPORT.sarif"
-    fi
-  fi
-}
-
 #
 # Setup audit configuration
 #
@@ -135,15 +104,7 @@ echo "[*] Setting up audit configuration"
 mkdir -p .42c/
 cp /42c-audit-config.yaml .42c/conf.yaml
 
-set
-
-# If the user has not specified a file to analyze
-if [ -n "${INPUT_OPENAPI_FILE}" ]; then
-  echo "[*] Starting discovery run"
-  discovery_run
-
-# If variable is set then run discovery run
-else
-  echo "[*] Running for file ${INPUT_OPENAPI_FILE}"
-  discovery_run
-fi
+#
+# Run discovery
+#
+discovery_run
