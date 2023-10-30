d2ea7f7806144fa74d28e55e93e51ab778ca480c
cr0hn <cr0hn@cr0hn.com>
2023-10-30 14:28:49 +0100

    fix

--- entrypoint_github_actions_audit.py
+++ entrypoint_github_actions_audit.py
@@ -217,6 +217,8 @@ def discovery_run(running_config: RunningConfiguration, base_dir: str, binaries:
 
             if upload_to_code_scanning_results.returncode != 0:
                 print(f"[!] Unable to upload SARIF report to GitHub code scanning")
+                print(upload_to_code_scanning_results.stderr)
+                print(upload_to_code_scanning_results.stdout)
                 continue
 
 
