d1bf71a53ecf529a42fd612f77d74afcc7ee648b
cr0hn <cr0hn@cr0hn.com>
2023-11-23 16:17:52 +0100

    imp - now action doesn't call directly a subprocess, instead

--- entrypoint_github_actions_audit.py
+++ entrypoint_github_actions_audit.py
@@ -7,8 +7,11 @@ import logging
 from dataclasses import dataclass
 
 from xliic_sdk.audit import load_metadata
+from xliic_sdk.helpers import ExecutionError
 from xliic_sdk.audit.report import AuditReport
-from xliic_sdk.helpers import get_binary_path, execute, ExecutionError
+from xliic_cli.audit.reports.sarif.merge_sarif.app import merge_sarif_files
+from xliic_cli.audit.reports.sarif.convert_to_sarif.app import convert_to_sarif
+from xliic_cli.audit.run.local_run import run_audit_locally, AuditExecutionConfig
 from xliic_sdk.vendors import github_running_configuration, display_header, upload_sarif
 
 logger = logging.getLogger(__name__)
@@ -36,6 +39,8 @@ class RunningConfiguration:
     github_ref: str = None
     github_sha: str = None
 
+    input_openapi_path: str = None
+
     @classmethod
     def from_github(cls) -> "RunningConfiguration":
 
@@ -49,6 +54,7 @@ class RunningConfiguration:
                 "export-as-pdf": "str",
                 "token": "str",
                 "audit-reports-dir": "str",
+                "openapi-path": "str",
             },
             envs={
                 "github_repository": "str",
@@ -60,6 +66,7 @@ class RunningConfiguration:
 
         o = cls(
             log_level=config["log-level"],
+            input_openapi_path=config["openapi-path"],
             data_enrich=config["data-enrich"],
             enforce=config["enforce-sqg"],
             upload_to_code_scanning=config["upload-to-code-scanning"],
@@ -107,33 +114,17 @@ RunningConfiguration:
         if self.log_level:
             self.log_level = self.log_level.lower()
 
+        if not self.input_openapi_path:
+            self.input_openapi_path = os.getcwd()
 
-def discovery_run(running_config: RunningConfiguration, binaries: str):
-    base_dir = os.getcwd()
 
+def discovery_run(running_config: RunningConfiguration):
+    upload_sarif(1, 2, 3, 4)
     if running_config.audit_reports_dir:
         output_directory = running_config.audit_reports_dir
 
     else:
-        output_directory = os.path.join(base_dir, uuid.uuid4().hex)
-
-    #
-    # Run 42Crunch cli audit
-    #
-    audit_cmd = [
-        "42ctl",
-        "audit",
-        "run",
-        "local",
-        "-b", binaries,
-        "-i", base_dir,
-        "-r", output_directory,
-        "-c",  # Copy original OpenAPI file that generated report
-        "--github-user", running_config.github_repository_owner,
-        "--github-org", running_config.github_organization,
-        "--log-level", running_config.log_level,
-        "--github-repo", running_config.github_repository
-    ]
+        output_directory = os.path.join(running_config.input_openapi_path, uuid.uuid4().hex)
 
     # Show, only in debug, audit parameters
     logger.debug(f"Running audit with the following parameters:")
@@ -142,34 +133,48 @@ def discovery_run(running_config: RunningConfiguration, binaries: str):
     logger.debug(f"Using GitHub repo: {running_config.github_repository.split('/')[1]}")
     logger.debug(f"Using log level: {running_config.log_level}")
     logger.debug(f"Using '{output_directory}' as result directory")
-    logger.debug(f"Using '{base_dir}' as input directory to look for OpenAPI files")
+    logger.debug(f"Using '{running_config.input_openapi_path}' as input directory to look for OpenAPI files")
 
-    if running_config.data_enrich:
-        audit_cmd.append("--enrich")
+    execution_config = AuditExecutionConfig(
+        github_repo=running_config.github_repository,
+        enrich=running_config.data_enrich,
+        github_org=running_config.github_organization,
+        github_user=running_config.github_repository_owner,
+        log_level=running_config.log_level,
+        enrich_mode=running_config.data_enrich
+
+    )
+
+    reports = {}
 
     try:
-
-        logger.debug(f"Executing audit command:")
-        logger.debug(f"{' '.join(audit_cmd)}")
-
-        stdout_response, stderr_response = execute(audit_cmd, True)
-    except ExecutionError as e:
-        logger.error(display_header("Audit command failed", str(e)))
+        for msg, report_path in run_audit_locally(
+                open_api_file_or_path=running_config.input_openapi_path,
+                output_file_or_dir=output_directory,
+                include_metadata=True,
+                audit_config=execution_config
+        ):
+            reports[os.path.basename(report_path)] = msg
+    except Exception as e:
+        logger.error(f"[!] {str(e)}")
         exit(1)
+
+    results_files = os.listdir(output_directory)
+
+    #
+    # Show, only in debug, Found reports
+    #
+    logger.debug(f"Analyzed {len(results_files)} files")
+    logger.debug(f"Reports are generated at: '{output_directory}'")
+    logger.debug(f"Found {len(results_files)} files in '{output_directory}'")
+    for report in results_files:
+        logger.debug(f" - {report}")
+    logger.debug(f"Converting the audit reports to SARIF")
 
     #
     # Convert to SARIF
     #
     sarif_reports = []
-    results_files = os.listdir(output_directory)
-
-    # Show, only in debug, Found reports
-    logger.debug(f"Reports are generated at: '{output_directory}'")
-    logger.debug(f"Found {len(results_files)} files in '{output_directory}'")
-    for report in results_files:
-        logger.debug(f" - {report}")
-
-    logger.debug(f"Converting the audit reports to SARIF")
     for report in os.listdir(output_directory):
 
         # Try to locate report files
@@ -194,10 +199,14 @@ def discovery_run(running_config: RunningConfiguration, binaries: str):
 
         # User log info
         report_obj = AuditReport.from_file(report_path)
+
         logger.info(f"Audited '{openapi_file}'")
-        logger.info(f"Issues Found: {report_obj.total_issues}")
         logger.info(f"Global score: {report_obj.score}")
-        logger.info(f"---")
+        logger.info(f"Issues Found: {report_obj.total_issues}")
+
+        # Display Quotas
+        logger.info(f"\n{reports[report]}\n\n")
+        # logger.info(f"---")
 
         logger.debug(f"Using '{openapi_file}' as input OpenAPI file for the SARIF generator")
 
@@ -205,43 +214,13 @@ def discovery_run(running_config: RunningConfiguration, binaries: str):
         sarif_file = f"{report_path}.sarif"
         logger.debug(f"Using '{sarif_file}' as output SARIF file")
 
-        cmd = [
-            "42ctl",
-            "audit",
-            "report",
-            "sarif",
-            "convert",
-            "-r", report_path,
-            "-a", openapi_file,
-            "-o", sarif_file
-        ]
-
-        logger.debug(f"Executing convert to SARIF command:")
-        logger.debug(f"{' '.join(cmd)}")
-
         try:
-            execute(cmd)
-
-            sarif_reports.append(sarif_file)
+            convert_to_sarif(openapi_file, report_path, sarif_file)
         except ExecutionError as e:
-            logger.error(display_header("Convert to SARIF command failed", str(e)))
-            continue
+            print(f"[!] {str(e)}")
+            exit(1)
 
-        # TODO: Enforce security issues
-        # logger.debug(
-        #     f"Enforce security issues is {'enabled' if running_config.enforce else 'disabled'}"
-        # )
-        # if running_config.enforce:
-        #     logger.debug(f"Checking for security issues in '{sarif_file}'")
-        #
-        #     ## If enforce is set to false, we'll fail if security issues were found
-        #     found, issues = is_security_issues_found(sarif_file)
-        #
-        #     logger.debug(f"Security issues found: {found}")
-        #
-        #     if found:
-        #         logger.error(display_header("Security issues found", f"{issues} issues found"))
-        #         sys.exit(1)
+        sarif_reports.append(sarif_file)
 
     #
     # Merge SARIF files
@@ -252,28 +231,14 @@ def discovery_run(running_config: RunningConfiguration, binaries: str):
     if running_config.sarif_report:
         sarif_report_name = running_config.sarif_report
     else:
-        sarif_report_name = os.path.join(base_dir, "audit.sarif")
+        sarif_report_name = os.path.join(running_config.input_openapi_path, "audit.sarif")
 
     logger.debug(f"Merging SARIF files into '{sarif_report_name}'")
 
     for s in sarif_reports:
         logger.debug(f" - {s}")
 
-    cmd = [
-        "42ctl",
-        "audit",
-        "report",
-        "sarif",
-        "merge",
-        "-o", sarif_report_name,
-        *sarif_reports
-    ]
-
-    try:
-        execute(cmd)
-    except ExecutionError as e:
-        logger.error(display_header("Merge SARIF files command failed", str(e)))
-        exit(1)
+    merge_sarif_files(sarif_reports, sarif_report_name)
 
     #
     # Upload to GitHub code scanning
@@ -295,11 +260,6 @@ def discovery_run(running_config: RunningConfiguration, binaries: str):
 
 
 def main():
-    try:
-        binary_path = get_binary_path()
-    except ExecutionError as e:
-        logger.error(display_header("Unable to get 42c-ast binary", str(e)))
-        exit(1)
 
     try:
         running_config = RunningConfiguration.from_github()
@@ -320,7 +280,7 @@ def main():
     # -------------------------------------------------------------------------
 
     # Run discovery
-    discovery_run(running_config, binary_path)
+    discovery_run(running_config)
 
 
 # Main script execution
